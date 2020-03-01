b292ed5ff23405ce374ef774fcacd940ae66e00f
Andrew den Hertog <andrew.denhertog@gmail.com>
2020-03-01 11:22:28 +1000

    suggested changes

--- src/application-container.ts
+++ src/application-container.ts
@@ -4,16 +4,25 @@ import { LoggerModule } from '@node-ts/logger-core'
 import { WinstonModule } from '@node-ts/logger-winston'
 import { HandlersModule } from './handlers/handlers-module'
 import { BusWorkflowModule } from '@node-ts/bus-workflow'
+import {
+  BUS_RABBITMQ_SYMBOLS,
+  BusRabbitMqModule,
+  RabbitMqTransportConfiguration
+} from '@node-ts/bus-rabbitmq'
 
-export class ApplicationContainer extends Container {
-  constructor () {
-    super()
-    this.load(
-      new LoggerModule(),
-      new WinstonModule(),
-      new BusModule(),
-      new BusWorkflowModule(),
-      new HandlersModule()
-    )
-  }
+const rabbitConfiguration: RabbitMqTransportConfiguration = {
+  queueName: 'accounts-application-queue',
+  connectionString: 'amqp://guest:guest@localhost'
 }
+
+export const container = new Container()
+container.load(new LoggerModule())
+container.load(new BusModule())
+container.load(new BusRabbitMqModule())
+container.load(new WinstonModule())
+container.load(new BusWorkflowModule())
+container.load(new HandlersModule())
+
+container
+  .bind(BUS_RABBITMQ_SYMBOLS.TransportConfiguration)
+  .toConstantValue(rabbitConfiguration)
